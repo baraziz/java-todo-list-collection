b2f5fdc81768fc74e5e406d9d0dd3341410b7ca0
baraziz <baharudinazizcode@gmail.com>
2025-04-08 16:55:07 +0700

    update collection.app file

--- src/main/java/todo/collection/App.java
+++ src/main/java/todo/collection/App.java
@@ -1,10 +1,15 @@
 package todo.collection;
 
+import todo.view.View;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
+        View view = new View();
+
+        view.showMenu();
     }
 }
